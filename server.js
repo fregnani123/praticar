1e1ff8fe48ac811f2f680b7eb072c9156c3c8041
fregnani123 <fabianofregnani@hotmail.com>
2023-05-11 19:23:45 -0300

    studos

--- server.js
+++ server.js
@@ -6,26 +6,20 @@
 
 
 
+const exp = require('constants');
 const express = require('express');
 const app = express();
 const path = require('path');
 
 const PORT = 3000;
 
-app.use(express.static(path.resolve('index',__dirname, 'views')));
+app.use(express.static(path.resolve("index",__dirname,'views')))
 
-app.get('/pagina2', (req, res)=>{
-    res.sendFile(path.join(__dirname,'views', 'pagina2.html'))
+app.get('/pagina2',(req,res)=>{
+    res.sendFile(path.join(__dirname,'views','pagina2.html'))
 })
-
-app.get('/pagina3', (req, res) => {
-    res.sendFile(path.join(__dirname,'views', 'pagina3.html'))
-})
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-
 
 app.listen(3000, () => {
    console.log(`servidor rodando na ${PORT}`)
 })
+
