d774b4eab3e5b1b8c0d4eca68aac238f670758cc
fregnani123 <fabianofregnani@hotmail.com>
2023-05-14 18:39:11 -0300

    estudos

--- server.js
+++ server.js
@@ -13,16 +13,17 @@ const path = require('path');
 
 const PORT = 3000;
 
-app.use(express.static(path.resolve("index",__dirname,'views')))
+app.use(express.static(path.resolve(__dirname,'public')));
 
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, 'views','index.html'))
+})
 app.get('/pagina2',(req,res)=>{
     res.sendFile(path.join(__dirname,'views','pagina2.html'))
 });
 app.get('/pagina3',(req,res)=>{
     res.sendFile(path.join(__dirname,'views','pagina3.html'))
 });
-
-
 
 app.listen(3000, () => {
    console.log(`servidor rodando na ${PORT}`)
